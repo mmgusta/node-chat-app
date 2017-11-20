ed446cbe626e1edad2c2cb2bdd4e9dd82cbc2b31
mmgusta <gustavo.marinelli@gmail.com>
2017-11-20 13:25:21 -0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connect to server');
-
-    socket.emit('createMessage', {
-        from: 'Paola',
-        text: 'Hey'
-    });
 });
 
 socket.on('disconnect', function () {
